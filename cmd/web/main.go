ed260a9fbb99d1da82289fc687232042ca5acca2
Murtadha <78985176+MurtadhaInit@users.noreply.github.com>
2025-03-30 01:02:03 +0300

    Finally, within this initial phase, a skeleton yet functional app is defined (with the application struct) and instantiated in the main function. - So far the application struct only includes a structured logger and a products model to be injected in the form of pointers. - A basic CLI configuration is defined. It allow the change of the base address the app runs on and the DSN to be provided to the MySQL driver. - The logger is instantiated and configured and so does the database connection pool. - The app is started (with the serve mux router being passed) and this is logged appropriately.

---
+++ cmd/web/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"database/sql"
+	"flag"
+	"log/slog"
+	"net/http"
+	"os"
+
+	"e-store-backend/internal/models"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type application struct {
+	logger   *slog.Logger
+	products *models.ProductModel
+}
+
+func main() {
+	type config struct {
+		addr string
+		dsn  string
+	}
+	var cfg config
+	flag.StringVar(&cfg.addr, "addr", ":4210", "HTTP network address")
+	flag.StringVar(&cfg.dsn, "dsn", "dev_user:dev_password@tcp(127.0.0.1:6033)/ecomm?parseTime=true", "MySQL data source name")
+	flag.Parse()
+
+	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+		AddSource: true,
+		Level:     slog.LevelDebug,
+	}))
+
+	db, err := openDB(cfg.dsn)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+	defer db.Close()
+
+	app := &application{
+		logger:   logger,
+		products: &models.ProductModel{DB: db},
+	}
+
+	app.logger.Info("Starting server", slog.String("addr", cfg.addr))
+	err = http.ListenAndServe(cfg.addr, app.routes())
+	app.logger.Error(err.Error())
+	os.Exit(1)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	return db, nil
+}
