ea290ddc0ee1c22551b07873e89da37cfc91dfe8
Murtadha <78985176+MurtadhaInit@users.noreply.github.com>
2025-06-07 14:08:17 +0300

    Basic skeleton for testing purposes has been created for the required products handlers as per the API routes

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,23 +1,53 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 func (app *application) productView(w http.ResponseWriter, r *http.Request) {
+	// product, err := app.queries.GetProduct(r.Context(), id)
+	// if err != nil {
 
+	// }
 }
 
-func (app *application) productCreate(w http.ResponseWriter, r *http.Request) {
+func (app *application) productAdd(w http.ResponseWriter, r *http.Request) {
+	// get the category id from the category name
 
+	// result, _ := app.queries.AddProduct(r.Context(), repository.AddProductParams{
+	// 	Title:              "Ginger",
+	// 	ProductDescription: "My amazing ginger product",
+	// 	Category:           sql.NullInt32{Int32: 1, Valid: true},
+	// 	Price:              "33.2",
+	// 	ImageUrl:           "https://yoo.com",
+	// 	StockQuantity:      33,
+	// })
+	// println(result.LastInsertId())
 }
 
 func (app *application) productEdit(w http.ResponseWriter, r *http.Request) {
-
+	// result, err := app.queries.EditProduct(r.Context(), repository.EditProductParams{
+	// 	ProductID: ,
+	// })
 }
 
 func (app *application) productDelete(w http.ResponseWriter, r *http.Request) {
-
+	// err := app.queries.DeleteProduct(r.Context(), 2)
 }
 
 func (app *application) productLatest(w http.ResponseWriter, r *http.Request) {
 
+}
+
+func (app *application) productAll(w http.ResponseWriter, r *http.Request) {
+	products, err := app.queries.GetAllProducts(r.Context())
+	if err != nil {
+		app.serverError(w, r, err)
+		return
+	}
+
+	for _, product := range products {
+		fmt.Fprintf(w, "Product: %s\n", product.Title)
+	}
 }
