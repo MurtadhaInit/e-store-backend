f25e9aefec720b8d69283a2ac59717167c9dd819
Murtadha <78985176+MurtadhaInit@users.noreply.github.com>
2025-06-13 19:43:45 +0300

    Refactored and improved the clientError helper function to optionally include a message parameter representing a custom message to be returned along with the HTTP status code. If omitted, the message returned is going to be the textual representation of the supplied status code (the default behaviour).

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -16,6 +16,10 @@ func (app *application) serverError(w http.ResponseWriter, r *http.Request, err 
 	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 }
 
-func (app *application) clientError(w http.ResponseWriter, status int) {
-	http.Error(w, http.StatusText(status), status)
+func (app *application) clientError(w http.ResponseWriter, status int, message ...string) {
+	msg := http.StatusText(status)
+	if len(message) > 0 && message[0] != "" {
+		msg = message[0]
+	}
+	http.Error(w, msg, status)
 }
